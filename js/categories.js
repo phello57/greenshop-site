d0a4784a838060b2ddd116f7d3af2a40e1a50357
Artem Lobanchikov <103268341+phello57@users.noreply.github.com>
2023-01-26 01:00:24 +0500

    Add files via upload

---
+++ js/categories.js
@@ -0,0 +1,38 @@
+export const countCategories = [
+  {
+    kind: "House Plants",
+    count: 1,
+  },
+  {
+    kind: "Potter Plants",
+    count: 5,
+  },
+  {
+    kind: "Seeds",
+    count: 0,
+  },
+  {
+    kind: "Small Plants",
+    count: 1,
+  },
+  {
+    kind: "Big Plants",
+    count: 0,
+  },
+  {
+    kind: "Succulents",
+    count: 0,
+  },
+  {
+    kind: "Trerrariums",
+    count: 1,
+  },
+  {
+    kind: "Gardering",
+    count: 0,
+  },
+  {
+    kind: "Accessories",
+    count: 1,
+  },
+];
